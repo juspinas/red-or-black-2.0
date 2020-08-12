db4aa642532b1d6b77fadbcc036816546f8a5281
juspinas <justin.espinas@gmail.com>
2020-08-12 20:29:31 +1200

    first response from API for new deck

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,27 @@
-document.getElementById("redButton").addEventListener("click", function(){
+function getDeck() {
+    const fetchPromise = fetch('/newDeck'); 
+    const streamPromise = fetchPromise.then((response) => response.json()); 
+    streamPromise.then((data) => showDeck(data));
+}
+
+function showDeck(deck) {
+    alert(deck);
+}
+
+
+document.getElementById("redButton").addEventListener("click", function () {
     document.getElementById("redBlackContainer").classList.toggle("is-hidden");
     document.getElementById("nextButtonContainer").classList.toggle("is-hidden");
 });
-document.getElementById("blackButton").addEventListener("click", function(){
+document.getElementById("blackButton").addEventListener("click", function () {
     document.getElementById("redBlackContainer").classList.toggle("is-hidden");
     document.getElementById("nextButtonContainer").classList.toggle("is-hidden");
 });
-document.getElementById("nextButton").addEventListener("click", function(){
+document.getElementById("nextButton").addEventListener("click", function () {
     document.getElementById("redBlackContainer").classList.toggle("is-hidden");
     document.getElementById("nextButtonContainer").classList.toggle("is-hidden");
 });
+
+window.onload = function() {
+    getDeck();
+};
