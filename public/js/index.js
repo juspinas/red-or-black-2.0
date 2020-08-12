52fd4bb8c807811ff86c57887ea051cc98860db0
juspinas <justin.espinas@gmail.com>
2020-08-12 19:34:46 +1200

    Added buttons with minimum functionality

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+document.getElementById("redButton").addEventListener("click", function(){
+    document.getElementById("redBlackContainer").classList.toggle("is-hidden");
+    document.getElementById("nextButtonContainer").classList.toggle("is-hidden");
+});
+document.getElementById("blackButton").addEventListener("click", function(){
+    document.getElementById("redBlackContainer").classList.toggle("is-hidden");
+    document.getElementById("nextButtonContainer").classList.toggle("is-hidden");
+});
+document.getElementById("nextButton").addEventListener("click", function(){
+    document.getElementById("redBlackContainer").classList.toggle("is-hidden");
+    document.getElementById("nextButtonContainer").classList.toggle("is-hidden");
+});
