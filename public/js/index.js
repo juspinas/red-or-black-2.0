a7e037d0cc6edc163ea554e6ff0474e817d880f0
juspinas <justin.espinas@gmail.com>
2020-08-14 00:07:23 +1200

    Added restart button after deck runs out

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+// getting a shuffled new deck
+const lololol = 10;
 function newDeck() {
     document.getElementById("cardCount").innerHTML = "52";
     document.getElementById("pointCount").innerHTML = "0";
@@ -10,6 +12,7 @@ function storeDeck(deck) {
     document.getElementById("deckId").innerHTML = deck.deckId;
 }
 
+// Updates scores as guess is correct
 function updateScore(suit,guess) {
     if (guess.includes(suit)) {
         let current = parseInt(document.getElementById("pointCount").textContent);
@@ -17,6 +20,7 @@ function updateScore(suit,guess) {
     }
 }
 
+// draws a card from the current deckId
 function flipCard(deckId, guess) {
     const fetchPromise = fetch("https://deckofcardsapi.com/api/deck/"+ deckId +"/draw/?count=1",
         {
@@ -33,34 +37,33 @@ function showCard(card,guess) {
     document.getElementById("cardImage").innerHTML = "<img id='cardDisplay' src='" + card.cards[0].image + "'>";
     
     if (card.remaining == "0") {
-        document.getElementById("restartButtonContainer").classList.toggle("is-hidden");
+        document.getElementById("restartButtonContainer").classList.remove("is-hidden");
     } else {
-        document.getElementById("nextButtonContainer").classList.toggle("is-hidden");
+        document.getElementById("nextButtonContainer").classList.remove("is-hidden");
     }
 }
 
 // button listeners
 document.getElementById("redButton").addEventListener("click", function () {
-    document.getElementById("redBlackContainer").classList.toggle("is-hidden");
+    document.getElementById("redBlackContainer").classList.add("is-hidden");
     const deckId = document.getElementById("deckId").textContent;
     flipCard(deckId,["HEARTS","DIAMONDS"]);
 });
 document.getElementById("blackButton").addEventListener("click", function () {
-    document.getElementById("redBlackContainer").classList.toggle("is-hidden");
+    document.getElementById("redBlackContainer").classList.add("is-hidden");
     const deckId = document.getElementById("deckId").textContent;
     flipCard(deckId,["CLUBS","SPADES"]);
 });
 document.getElementById("nextButton").addEventListener("click", function () {
-    document.getElementById("redBlackContainer").classList.toggle("is-hidden");
-    document.getElementById("nextButtonContainer").classList.toggle("is-hidden");
+    document.getElementById("redBlackContainer").classList.remove("is-hidden");
+    document.getElementById("nextButtonContainer").classList.add("is-hidden");
     document.getElementById("cardImage").innerHTML = "<img id='cardDisplay' src='https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_1024x1024.png?v=1523371937'>";
 });
-// document.getElementById("restartButton").addEventListener("click", function () {
-//     document.getElementById("restartButtonContainer").classList.toggle("is-hidden");
-//     document.getElementById("redBlackContainer").classList.toggle("is-hidden");
-//     newDeck();
-// });
-
+document.getElementById("restartButton").addEventListener("click", function () {
+    document.getElementById("restartButtonContainer").classList.add("is-hidden");
+    document.getElementById("redBlackContainer").classList.remove("is-hidden");
+    newDeck();
+});
 
 // dropdown menu
 document.getElementById("dropdownButton").addEventListener("click", function () {
@@ -69,7 +72,7 @@ document.getElementById("dropdownButton").addEventListener("click", function () 
 });
 
 // restart modal functions
-document.getElementById("restartButton").addEventListener("click", function () {
+document.getElementById("restartButtonModal").addEventListener("click", function () {
     document.getElementById("restartModal").classList.toggle("is-active");
 });
 
